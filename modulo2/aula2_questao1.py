b24a9ce035aefef571c9d5398b4c09efb5b4b38d
Geison Oliveira <59226507+geisonoliveira@users.noreply.github.com>
2025-09-05 10:12:26 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,7 @@
+num1 = 5
+num2 = 2
+type(num1)
+type(num2)
+resultado = num1 / num2
+print(resultado)
+print(type(resultado))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,9 @@
+texto = "o resultado é"
+num1 = 10
+num2 = 3.5
+resultado = 10 + 3.5
+print(texto, num1, num2, resultado)
+print(type(texto))
+print(type(num1))
+print(type(num2))
+print(type(resultado))
